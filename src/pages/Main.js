1e8395055d078050706c4aa41a6944d222ce4d11
dimitrius-brest <d_der@mail.ru>
2021-04-02 11:23:03 +0300

    Получаем данные через fetch из API бэкенда

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 
 class MainPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false,
+            sellers: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://tovaryrp-from-vk.herokuapp.com/sellers')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    isLoaded: true,
+                    sellers: data
+                })
+                console.log(data);
+            })
+    }
+
     render() {
         return (
             <>
                 <h1>Главная страница</h1>
+                <p>Продавцы:</p>
+                <ul>
+                    {this.state.sellers.map(seller =>
+                        <li key={seller._id}>
+                            <img src={seller.vk_group_info.photo_50}/>
+                            {seller.name}
+                        </li>
+                    )}
+                </ul>
             </>
         )
     }
