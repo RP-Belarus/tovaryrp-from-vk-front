01fa05ac188f299ab4fc6a6a4556ec1592085f60
Dmitry Derevitsky <d_der@mail.ru>
2021-06-15 11:46:27 +0300

    Updates

---
+++ precache-manifest.a7dcec18b473aa8d205c0ddd67e54a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c39d1b9f15fe4fec895b4f9d2dc658",
+    "url": "/tovaryrp-from-vk-front/index.html"
+  },
+  {
+    "revision": "7fb16e9be53b16f81a81",
+    "url": "/tovaryrp-from-vk-front/static/css/2.db332a04.chunk.css"
+  },
+  {
+    "revision": "22eec64310d50c8dd60f",
+    "url": "/tovaryrp-from-vk-front/static/css/main.c5119f1d.chunk.css"
+  },
+  {
+    "revision": "7fb16e9be53b16f81a81",
+    "url": "/tovaryrp-from-vk-front/static/js/2.5bb8deae.chunk.js"
+  },
+  {
+    "revision": "cdef75879235130052acc2626c717c44",
+    "url": "/tovaryrp-from-vk-front/static/js/2.5bb8deae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22eec64310d50c8dd60f",
+    "url": "/tovaryrp-from-vk-front/static/js/main.8ea88817.chunk.js"
+  },
+  {
+    "revision": "97a9cb98c3d9681e0d64",
+    "url": "/tovaryrp-from-vk-front/static/js/runtime-main.471874b5.js"
+  }
+]);
