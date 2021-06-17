f8ed1303b3beace3e77fe67ecfd26a025e29c12c
Dmitry Derevitsky <d_der@mail.ru>
2021-06-17 11:31:55 +0300

    Добавлены ссылки в раздел "Ссылки"

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -2,7 +2,50 @@ import React, { Component } from 'react'
 
 class LinksPage extends Component {
     render() {
-        return (<h1>Ссылки</h1>)
+        return (
+            <>
+                <h1>Ссылки</h1>
+                <p>Идея Родовых поместий изложена в серии книг Владимира Мегре "Звенящие кедры России".</p>
+                <ul>
+                    <li><a
+                        href="https://vmegre.com"
+                        target="_blank">https://vmegre.com</a> - официальный сайт писателя Владимира Мегре</li>
+                    <li><a
+                        href="https://vk.com/vladimirmegre"
+                        target="_blank">https://vk.com/vladimirmegre</a> - страница Вконтакте Владимира Мегре</li>
+                </ul>
+                <p>Продукция под товарным знаком "Звенящие кедры России":</p>
+                <ul>
+                    <li><a
+                        href="https://megre.ru"
+                        target="_blank">https://megre.ru</a> - официальный сайт компании "Звенящие кедры"</li>
+                    <li><a
+                        href="https://vk.com/ringingcedars"
+                        target="_blank">https://vk.com/ringingcedars</a> - страница Вконтакте компании "Звенящие кедры"</li>
+                    <li><a
+                        href="http://kedra.by"
+                        target="_blank">http://kedra.by</a> - официальный дилер компании "Звенящие Кедры" в Беларуси</li>
+                    <li><a
+                        href="https://vk.com/kedraby"
+                        target="_blank">https://vk.com/kedraby</a> - официальный дилер компании "Звенящие Кедры" в Беларуси (Вконтакте)</li>
+                </ul>
+                <p>Поселения, состоящие из Родовых поместий:</p>
+                <ul>
+                    <li><a
+                        href="https://vk.com/rpbelrus"
+                        target="_blank">https://vk.com/rpbelrus</a> - Родовые поместья и поселения Беларуси (Вконтакте)</li>
+                    <li><a
+                        href="http://ecoby.info"
+                        target="_blank">http://ecoby.info</a> - Родовые поместья Беларуси, информационный портал</li>
+                    <li><a
+                        href="https://vk.com/club13589694"
+                        target="_blank">https://vk.com/club13589694</a> - Каталог поселений Родовых поместий</li>
+                    <li><a
+                        href="https://vk.com/tovary_rp_bel"
+                        target="_blank">https://vk.com/tovary_rp_bel</a> - Товары Родовых поместий Беларуси</li>
+                </ul>
+            </>
+        )
     }
 }
 
