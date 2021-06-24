78129166ca17a0eb07c74caf82766b1e51cf0c19
Dmitry Derevitsky <d_der@mail.ru>
2021-06-24 13:11:39 +0300

    Доработана карточка товара, добавлен переход по ссылке "Подробнее"

--- src/components/Products/ProductItem/ProductItem.js
+++ src/components/Products/ProductItem/ProductItem.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 
 const productItem = props => (
-    <Card style={{ width: '12rem', height: '16rem' }} className="float-left">
-        <Card.Body>
+    <Card style={{ width: '12rem', height: '18rem' }} className="float-left">
+        <Card.Body style={{ overflow: 'auto' }}>
             <Card.Img src={props.product.thumb_photo} />
-            <Card.Title>
-                {props.product.title}
-            </Card.Title>
             <Card.Text>
-                Цена: {props.product.price.text}
+                {props.product.title}
             </Card.Text>
+        </Card.Body>
+        <Card.Body>
+            Цена: {props.product.price.text}<br/>
+            <a href={'http://vk.com/product' + props.product.owner_id + '_' + props.product.id} target="_blank">
+                Подробнее
+            </a>
         </Card.Body>
     </Card>
 )
